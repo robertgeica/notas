5dee2881e3b91224bc3a1fed811c050a6f8a7a40
Robert Geica <geicarobert@gmail.com>
2021-06-15 13:04:04 +0300

    delete cateory mutation

--- src/mutations/deleteCategory.js
+++ src/mutations/deleteCategory.js
@@ -1,21 +1,30 @@
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@vue/apollo-composable';
+import useCategoryState from '../store/useCategoryState';
 
 export default function useDeleteCategoryMutation(id) {
-  const { mutate } = useMutation(
+  const query = gql`
+    query {
+      categories {
+        id
+        categoryName
+        notes {
+          noteTitle
+          noteBody
+        }
+      }
+    }
+  `;
+
+  const { mutate: deleteCategory } = useMutation(
     gql`
       mutation deleteCategory($id: ID!) {
         deleteCategory(id: $id) {
           id
         }
       }
-    `,
-    () => ({
-      variables: {
-        id
-      },
-    })
+    `
   );
-  
-  return { deleteCategory: (id) => mutate(id) };
+
+  return { deleteCategory: (id) => deleteCategory(id) };
 }
